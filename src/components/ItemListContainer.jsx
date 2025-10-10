ff73f2a46550ea4a053ee93c681cc7dd6ccad98a
Gonzalo-Recoba <gonzalo.recoba@gmail.com>
2025-10-09 22:15:56 -0300

    Se agrega Carousel

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Carousel from './Carousel'
 
 const ItemListContainer = () => {
     return (
         <main>
+            <Carousel/>
             <h1 className='text-center mt-5'>Bienvenidos a Agentur</h1>
             <p className='text-center mt-3'>Tu próximo viaje comienza aquí</p>
         </main>
